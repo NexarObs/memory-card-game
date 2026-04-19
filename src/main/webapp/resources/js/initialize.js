24c5b0c0bd3d59ab0e400f2ecc0ceb792dfdd789
NexarObs <ouabas.mehdi.s@gmail.com>
2026-04-19 10:41:21 +0100

    feat: added the basic default Java spring structure with the proper gitignore

---
+++ src/main/webapp/resources/js/initialize.js
@@ -0,0 +1,12 @@
+/**
+ * Práctica de Ferramentas de Desenvolvemento javascript initialization.
+ * Copyright 2020 Javier Parapar
+ * Licensed under the MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * 
+ * Loads all the Javascript functions used by the site.
+ */
+
+/**
+ * Initializes scripts when the document loads.
+ */
+$(document).ready(function() {});

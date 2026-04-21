6f237b859be27a1d9996150460e1798f7e210624
NexarObs <ouabas.mehdi.s@gmail.com>
2026-04-22 00:12:40 +0100

    feat: added the static UI html css component along with the controller for the game page

--- src/main/java/com/jee/memorycardgame/controller/GameController.java
+++ src/main/java/com/jee/memorycardgame/controller/GameController.java
@@ -1,5 +1,22 @@
 package com.jee.memorycardgame.controller;
 
+import com.jee.memorycardgame.model.GameModel;
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class GameController {
-    
+
+    @GetMapping("/game")
+    public String showGame(HttpSession session, Model model) {
+
+        GameModel activeGame =
+                (GameModel) session.getAttribute("activeGame");
+
+        model.addAttribute("game", activeGame);
+
+        return "game";
+    }
 }

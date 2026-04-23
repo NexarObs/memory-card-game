583964fdd9751982f1bcee6cc19916518a92aef5
NexarObs <ouabas.mehdi.s@gmail.com>
2026-04-23 13:30:34 +0100

    fix: made the dynamic route in the GameController work

--- src/main/java/com/jee/memorycardgame/controller/GameController.java
+++ src/main/java/com/jee/memorycardgame/controller/GameController.java
@@ -1,22 +1,50 @@
 package com.jee.memorycardgame.controller;
 
 import com.jee.memorycardgame.model.GameModel;
+import com.jee.memorycardgame.service.GameService;
+
 import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class GameController {
 
-    @GetMapping("/game")
-    public String showGame(HttpSession session, Model model) {
+    @Autowired
+    private GameService gameService;
 
-        GameModel activeGame =
-                (GameModel) session.getAttribute("activeGame");
 
-        model.addAttribute("game", activeGame);
+    @GetMapping("/game{id}")
+    public String showGame(
+            @PathVariable int id,
+            HttpSession session,
+            Model model
+    ) {
+
+        Integer userId =
+                (Integer) session.getAttribute("userId");
+
+        if (userId == null)
+            return "redirect:/login";
+
+
+        GameModel game =
+                gameService.loadGame(id);
+
+
+        session.setAttribute("activeGame", game);
+
+
+        model.addAttribute("game", game);
+
+        model.addAttribute("userId", userId);
+
 
         return "game";
     }
+
 }
